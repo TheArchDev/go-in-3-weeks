82803eb78d13c6467f7ddf7b8a89b527d389ea6a
Alex Archer <archeralex@gmail.com>
2021-10-12 18:30:16 +0100

    Fix line break typo

--- exercise1/exercise1.go
+++ exercise1/exercise1.go
@@ -22,7 +22,7 @@ func main() {
 
 	fmt.Printf("Hello, fellow %s Gophers!\n", location)
 	fmt.Printf("My name is %s and I'm from %s.\n", name, from)
-	fmt.Printf("By the time %d o'clock EST comes around, we'll know how to code in Go!\\n", n)
+	fmt.Printf("By the time %d o'clock EST comes around, we'll know how to code in Go!\n", n)
 	fmt.Printf("Here's a Go proverb: %s\n", proverb)
 	fmt.Println("Let's get started!")
 }
